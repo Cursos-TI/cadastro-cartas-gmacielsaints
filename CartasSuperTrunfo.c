1e1eeff80a4ff4a253dfc4967f3f3d3bbfe210a5
Gean Maciel <geanplay20@gmail.com>
2025-09-14 22:37:18 +0000

    Criei o Codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,67 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  
+  char Estado1[20], Estado2[20], Codigo1[4], Codigo2[4], Cidade1[20], Cidade2[20];
+  int Populacao1, Populacao2, Tour1, Tour2;
+  float Area1, Area2, PIB1, PIB2;
 
-  // Área para entrada de dados
+// COLETANDO INFORMAÇÕES DA CARTA 1:
+printf("Infome o Estado 1:\n");
+scanf("%s", Estado1);
+printf("Infome o Codigo da Carta 1:\n");
+scanf("%3s", Codigo1);
+printf("Infome a Cidade 1:\n");
+scanf("%s", Cidade1);
+printf("Infome a Populacao da cidade 1:\n");
+scanf("%d", &Populacao1);  
+printf("Infome a Quantidade de Pontos Turisticos da cidade 1:\n");
+scanf("%d", &Tour1);
+printf("Infome o PIB da cidade 1:\n");
+scanf("%f", &PIB1);
+printf("Infome a Area da cidade 1:\n");
+scanf("%f", &Area1);
+
+// COLETANDO INFORMAÇÕES DA CARTA 2:
+printf("Infome o Estado 2:\n");
+scanf("%s", Estado2);
+printf("Infome o Codigo da Carta 2:\n");
+scanf("%3s", Codigo2);
+printf("Infome a Cidade 2:\n");
+scanf("%s", Cidade2);
+printf("Infome a Populacao da cidade 2:\n");
+scanf("%d", &Populacao2);  
+printf("Infome a Quantidade de Pontos Turisticos da cidade 2:\n");
+scanf("%d", &Tour2);
+printf("Infome o PIB da cidade 2:\n");
+scanf("%f", &PIB2);
+printf("Infome a Area da cidade 2:\n");
+scanf("%f", &Area2);
 
   // Área para exibição dos dados da cidade
+
+  // EXIBINDO INFORMAÇÕES DA CARTA 1:
+  printf("A Carta 01 possui as seguintes caracteristicas:\n");
+  printf("Estado: %s\n"
+        "Codigo: %3s\n"
+        "Cidade: %s\n"
+        "Populacao: %d\n"
+        "Pontos Turisticos: %d\n"
+        "PIB: %f\n"
+        "Area da Cidade: %f\n",
+        Estado1, Codigo1, Cidade1, Populacao1, Tour1, PIB1, Area1);
+
+ // EXIBINDO INFORMAÇÕES DA CARTA 2:
+  printf("A Carta 02 possui as seguintes caracteristicas:\n");
+  printf("Estado: %s\n"
+        "Codigo: %3s\n"
+        "Cidade: %s\n"
+        "Populacao: %d\n"
+        "Pontos Turisticos: %d\n"
+        "PIB: %f\n"
+        "Area da Cidade: %f\n",
+        Estado2, Codigo2, Cidade2, Populacao2, Tour2, PIB2, Area2);
 
 return 0;
 } 
