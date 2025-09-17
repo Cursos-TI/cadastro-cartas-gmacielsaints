6ad655a844593dcf017ac608521c89313f9b35ff
Gean Maciel <geanplay20@gmail.com>
2025-09-17 21:35:35 +0000

    Nivel Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,7 +5,7 @@ int main() {
   
   char Estado1[20], Estado2[20], Codigo1[4], Codigo2[4], Cidade1[20], Cidade2[20];
   int Populacao1, Populacao2, Tour1, Tour2;
-  float Area1, Area2, PIB1, PIB2;
+  float Area1, Area2, PIB1, PIB2, Dens1, Dens2, PPC1, PPC2;
 
 // COLETANDO INFORMAÇÕES DA CARTA 1:
 printf("Infome o Estado 1:\n");
@@ -20,8 +20,10 @@ printf("Infome a Quantidade de Pontos Turisticos da cidade 1:\n");
 scanf("%d", &Tour1);
 printf("Infome o PIB da cidade 1:\n");
 scanf("%f", &PIB1);
-printf("Infome a Area da cidade 1:\n");
+printf("Infome a Area da cidade 1 (km2):\n");
 scanf("%f", &Area1);
+Dens1 = Populacao1 / Area1;
+PPC1 = PIB1 / Populacao1;
 
 // COLETANDO INFORMAÇÕES DA CARTA 2:
 printf("Infome o Estado 2:\n");
@@ -36,8 +38,11 @@ printf("Infome a Quantidade de Pontos Turisticos da cidade 2:\n");
 scanf("%d", &Tour2);
 printf("Infome o PIB da cidade 2:\n");
 scanf("%f", &PIB2);
-printf("Infome a Area da cidade 2:\n");
+printf("Infome a Area da cidade 2 (km2):\n");
 scanf("%f", &Area2);
+Dens2 = Populacao2 / Area2;
+PPC2 = PIB2 / Populacao2;
+
 
   // Área para exibição dos dados da cidade
 
@@ -48,9 +53,11 @@ scanf("%f", &Area2);
         "Cidade: %s\n"
         "Populacao: %d\n"
         "Pontos Turisticos: %d\n"
-        "PIB: %f\n"
-        "Area da Cidade: %f\n",
-        Estado1, Codigo1, Cidade1, Populacao1, Tour1, PIB1, Area1);
+        "PIB Total: %.2f\n"
+        "PIB per capta da Cidade: %.2f\n"
+        "Area da Cidade (km2): %.3f\n"
+        "Densidade Demografica da Cidade(Pessoa(as) por km2): %.2f\n",
+        Estado1, Codigo1, Cidade1, Populacao1, Tour1, PIB1, PPC1, Area1, Dens1);
 
  // EXIBINDO INFORMAÇÕES DA CARTA 2:
   printf("A Carta 02 possui as seguintes caracteristicas:\n");
@@ -59,9 +66,11 @@ scanf("%f", &Area2);
         "Cidade: %s\n"
         "Populacao: %d\n"
         "Pontos Turisticos: %d\n"
-        "PIB: %f\n"
-        "Area da Cidade: %f\n",
-        Estado2, Codigo2, Cidade2, Populacao2, Tour2, PIB2, Area2);
+        "PIB Total: %.2f\n"
+        "PIB per capta da Cidade: %.2f\n"
+        "Area da Cidade (km2): %.3f\n"
+        "Densidade Demografica da Cidade(Pessoa(as) por km2): %.2f\n",
+        Estado2, Codigo2, Cidade2, Populacao2, Tour2, PIB2, PPC2, Area2, Dens2);
 
 return 0;
 } 
